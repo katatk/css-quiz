dc1b11d528bc152c8c56ba7ca6e51d7dfe0d7e6d
Kat <katatk@users.noreply.github.com>
2017-08-01 12:23:44 +1200

    fixed up some previous changes

--- quiz.js
+++ quiz.js
@@ -204,9 +204,59 @@ Calculate Score
 
 =========== */
 
+/* displays the score at the end and displays the incorrect questions in the format they appeared in the quiz + the user's incorrect answer and correct answer */
+function displayScore(incorrectQuestions, score, message) {
+    
+    /* build the html to display to the page */
+    var output = "<div class='box'>";
+    output += "<p> Congratulations on completing the quiz!</p>"
+    output += "<p> Your score is " + score + " out of " + questions.length + ". " + message + "</p><p>The following questions were incorrect:</p><ul>";
+    
+      // list the numbers of the incorrect questions
+    for (var y = 0; y < incorrectQuestions.length; y++) {
+        output += "<li class='question incorrect'>" + questions[incorrectQuestions[y].questionIndex].number + "</li>";
+
+    }
+    output += "</ul>";
+    output += "</div>";
+
+
+    // list the incorrect answers in full
+    for (x = 0; x < incorrectQuestions.length; x++) {
+
+        output += "<div class='box'>";
+        output += "<legend class='question-heading'>" + questions[incorrectQuestions[x].questionIndex].number + ". " + questions[incorrectQuestions[x].questionIndex].title + "</legend>";
+
+        for (var j = 0; j < questions[incorrectQuestions[x].questionIndex].answers.length; j++) {
+            output += "<div class='questions'>";
+            output += "<li class='question ";
+            if (j == questions[incorrectQuestions[x].questionIndex].correctAnswer) {
+                output += "correct'>";
+            } else if (j == incorrectQuestions[x].incorrectIndex) {
+                output += "incorrect'>";
+            } else {
+                output += "other'>";
+            }
+            output += "<label>" + questions[incorrectQuestions[x].questionIndex].answers[j] + "</label>";
+            output += "</li>";
+            output += "</div>";
+        }
+        output += "</div>";
+
+    }
+
+
+    document.getElementById("score-container").innerHTML = output;
+
+    /* hide the questions div and show the score div */
+    document.getElementById("two").style.display = 'block';
+    document.getElementById("one").style.display = 'none';
+
+}
+
+
 
 function calcScore() {
-
 
     var incorrectQuestions = [];
 
@@ -246,6 +296,7 @@ function calcScore() {
     }
 
     var message = "";
+    var output = "";
 
     if (score < 6) {
         message = "Oh dear... looks like you could use some more practice.";
@@ -264,70 +315,13 @@ function calcScore() {
         message = "Wow, you got 100%! You clever cookie you (;";
     }
 
-    displayScore(score);
-    displayIncorrectQuestions(incorrectQuestions);
+    displayScore(incorrectQuestions, score, message);
 }
 
-
-/* displays the score at the end and a list of incorrect questions */
-function displayScore(score) {
-
-    /* build the html to display to the page */
-    var output = "";
-    output += "<div class='box'>";
-    output += "<p> Congratulations on completing the quiz!</p>"
-    output += "<p> Your score is " + score + " out of " + questions.length + ". " + message + "</p><p>The following questions were incorrect:</p><ul>";
-
-    // list the numbers of the incorrect questions
-    for (var y = 0; y < incorrectQuestions.length; y++) {
-        output += "<li class='question incorrect'>" + questions[incorrectQuestions[y].questionIndex].number + "</li>";
-
-    }
-    output += "</ul>";
-    output += "</div>";
-
-}
-
-/* displays the incorrect questions in the format they appeared in the quiz + the user's incorrect answer and correct answer */
-function displayIncorrectQuestions(incorrectQuestions) {
-
-    // list the incorrect answers in full
-    for (x = 0; x < incorrectQuestions.length; x++) {
-
-        output += "<div class='box'>";
-        output += "<legend class='question-heading'>" + questions[incorrectQuestions[x].questionIndex].number + ". " + questions[incorrectQuestions[x].questionIndex].title + "</legend>";
-
-        for (var j = 0; j < questions[incorrectQuestions[x].questionIndex].answers.length; j++) {
-            output += "<div class='questions'>";
-            output += "<li class='question ";
-            if (j == questions[incorrectQuestions[x].questionIndex].correctAnswer) {
-                output += "correct'>";
-            } else if (j == incorrectQuestions[x].incorrectIndex) {
-                output += "incorrect'>";
-            } else {
-                output += "other'>";
-            }
-            output += "<label>" + questions[incorrectQuestions[x].questionIndex].answers[j] + "</label>";
-            output += "</li>";
-            output += "</div>";
-        }
-        output += "</div>";
-
-    }
-
-
-    document.getElementById("score-container").innerHTML = output;
-
-    /* hide the questions div and show the score div */
-    document.getElementById("two").style.display = 'block';
-    document.getElementById("one").style.display = 'none';
-
-}
 
 var currentQuestion = 1;
 
 function nextQuestion(e) {
-    debugger;
     // show previous question button from question 2 onwards
     if (currentQuestion >= 1) {
 
